02daebcb41cbeb0b8cf8cfa1ce04e4e7f19e5dfd
Jeziel Marques <jezielmarques7@gmail.com>
2020-02-04 02:59:43 -0300

    init mobile project

---
+++ mobile/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text, } from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+
+export function App() {
+  return (
+    <>
+      <View >
+        <Text>init</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
